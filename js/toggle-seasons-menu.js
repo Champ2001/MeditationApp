b738aa2c993350a032c3a90aa2cae5fcefbfc990
sarnava mohanta <sarnavamohanta2001@gmai.com>
2022-11-05 20:54:40 +0530

    first commit

---
+++ js/toggle-seasons-menu.js
@@ -0,0 +1,8 @@
+/* TOGGLE-SEASONS-MENU.JS */
+const openMenuBtn = document.querySelector(".toggle-menu"),
+  seasonsMenu = document.querySelector(".seasons");
+
+openMenuBtn.addEventListener("click", () => {
+  seasonsMenu.classList.toggle("open");
+  openMenuBtn.classList.toggle("rotate");
+});
